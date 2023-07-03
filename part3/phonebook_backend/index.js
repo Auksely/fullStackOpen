5115994a9e2ea2b62087ea22b6258e856ec0ad4a
Auksė Četrauskaitė <auksely@gmail.com>
2023-07-03 16:33:24 +0300

    add morgan

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,7 +1,16 @@
+const morgan=require('morgan')
 const express = require("express")
 const app = express()
 
 app.use(express.json())
+morgan.token('custom', (req) => {
+  return 'POST' === req.method ? JSON.stringify(req.body) : ' '
+})
+app.use(
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms :custom'
+  )
+)
 
 let persons =
     [
