04b87e83f34ee0cfa8212e8291344c271d1b74fd
Auksė Četrauskaitė <auksely@gmail.com>
2023-07-03 16:05:19 +0300

    add morgan

--- part3/stud/index.js
+++ part3/stud/index.js
@@ -1,7 +1,30 @@
+const morgan=require('morgan')
 const express = require('express')
 const app = express()
 
+morgan.token('custom', (req) => {
+  return 'POST' === req.method ? JSON.stringify(req.body) : ' '
+})
+
+// const requestLogger = (request, response, next) => {
+//   console.log('Method:', request.method)
+//   console.log('Path:  ', request.path)
+//   console.log('Body:  ', request.body)
+//   console.log('---')
+//   next()
+// }
+
+// const unknownEndpoint = (request, response) => {
+//   response.status(404).send({ error: 'unknown endpoint' })
+// }
+
 app.use(express.json())
+// app.use(requestLogger)
+app.use(
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms :custom'
+  )
+)
 
 let notes = [
     {
@@ -29,27 +52,21 @@ app.get('/api/notes', (request, response) => {
     response.json(notes)
 })
 
-// app.get('/api/notes/:id', (request, response) => {
-//     const id = Number(request.params.id)
-//     const note = notes.find(note => note.id === id)
-//     if (note) {
-//         response.json(note)
-//     }
-//     response.status(404).end()
-// })
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    if (note) {
+        response.json(note)
+    }
+    response.status(404).end()
+})
 
-// app.post('/api/notes', (request, response) => {
-//     const note = request.body
-//     console.log(note)
-//     response.json(note)
-//   })
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
 
-// app.delete('/api/notes/:id', (request, response) => {
-//     const id = Number(request.params.id)
-//     notes = notes.filter(note => note.id !== id)
-
-//     response.status(204).end()
-// })
+    response.status(204).end()
+})
 
 const generateId = () => {
     const maxId = notes.length > 0
@@ -77,6 +94,9 @@ const generateId = () => {
   
     response.json(note)
   })
+
+  // app.use(unknownEndpoint)
+
 
 const PORT = 3001
 app.listen(PORT)
